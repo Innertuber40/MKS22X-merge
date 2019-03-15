74c164e3f94107d0e6cb74558582ecbc8f83a8f7
bstringham <bstringham@stuy.edu>
2019-03-15 11:01:02 -0400

    Started work on the helper function

--- Merge.java
+++ Merge.java
@@ -4,8 +4,17 @@ public class Merge {
 		for (int i = 0; i < data.length; i++) {
 			fraud[i] = data[i];
 		}
-		for (int i = 0; i < fraud.length; i++) {
+		/*for (int i = 0; i < fraud.length; i++) {
 			System.out.print(fraud[i] + " ");
+		}*/
+		merge(data, fraud, 0, data.length - 1);
+	}
+	private static void merge(int[] data, int[] fraud, int low, int high) {
+		if (low >= high) {
+			return;
 		}
+		int breaker = (high - low) / 2;
+		merge(fraud, data, low, breaker);
+		merge(fraud, data, breaker + 1, high);
 	}
 }
