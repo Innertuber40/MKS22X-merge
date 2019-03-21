8a6fc5e10e886959e82e41fb7acdac79d68594e2
Innertuber <bstringham@stuy.edu>
2019-03-20 22:54:27 -0400

    Potentially finished merge

--- Merge.java
+++ Merge.java
@@ -10,11 +10,26 @@ public class Merge {
 		merge(data, fraud, 0, data.length - 1);
 	}
 	private static void merge(int[] data, int[] fraud, int low, int high) {
-		if (low >= high) {
+		if (high - low >= 10) {
+			for (int i = 1; i < data.length; i++) {                         // check each value only once      
+				int j = i;
+				int toInsert = data[j];                                     // save the value we are swapping back
+				while (j > 0 && data[j - 1] > toInsert) {                   // occurs until the end or a smaller element
+					data[j] = data[j - 1];                                // shift up
+					j--;                                                      // move on to the next pair
+				}
+				data[j] = toInsert;
+			}
 			return;
 		}
-		int breaker = (high - low) / 2;
+		/*for (int i = 0; i < fraud.length; i++) {
+			System.out.print(fraud[i] + " ");
+		}*/
+		int breaker = (high - low) / 2 + low;
 		merge(fraud, data, low, breaker);
 		merge(fraud, data, breaker + 1, high);
+		for (int i = low; i < high; i++) {
+			data[i] = fraud[i];
+		}
 	}
 }
--- Miver.java
+++ Miver.java
@@ -2,5 +2,8 @@ public class Miver {
 	public static void main(String[] args) {
 		int[] test1 = new int[] {0, 1, 1, 5, -3, -6, -3, 4, 8, 2343, 555555, -55555, -47, 1};
 		Merge.mergesort(test1);
+		for (int i = 0; i < test1.length; i++) {
+			System.out.print(test1[i] + " ");
+		}
 	}
 }
